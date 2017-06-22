9352da5e5fc5e986d1b3fbe5807aa28553b2ccd8
RossWoodruff <rosswoodruff@comcast.net>
2017-06-21 17:33:19 -0700

    Adding Dojo Survey with Validation Assignment

---
+++ Python/Week 2 Assignments/Dojo Survey with Validation/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, flash, session
+app = Flask(__name__)
+app.secret_key="Thisissecret"
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+	if len(request.form['name'])< 1 and len(request.form['comments'])>120:
+		flash("Name cannot be empty!")
+		flash('Comments are too long!')
+		return redirect('/')
+
+	if len(request.form['name']) < 1:
+		flash("Name cannot be empty")
+		return redirect('/')
+
+	if len(request.form['comments']) > 120:
+		flash('Comments are too long!')
+		return redirect('/')
+
+	return render_template('results.html', name = request.form['name'], location = request.form['location'], language = request.form['language'], comment = request.form['comments'])
+
+app.run(debug=True)
